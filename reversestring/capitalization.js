27293f25343c58da63b4946a13dcdc6926dd032c
Odongo006 <www.henryodongo006@gmail.com>
2020-02-17 17:19:10 +0300

    data structures

--- reversestring/capitalization.js
+++ reversestring/capitalization.js
@@ -1,8 +1,18 @@
+// function capitalize(str){
+//     str = str.split(' ')
+//     for(var i = 0; i<str.length; i++){
+//         str[i] = str[i][0].toUpperCase()+str[i].substr(1)
+//     }
+//     return str.join(' ')
+// }
+// console.log(capitalize('my wild dog is called a beast'))
+
 function capitalize(str){
-    str = str.split(' ')
-    for(var i = 0; i<str.length; i++){
-        str[i] = str[i][0].toUpperCase()+str[i].substr(1)
+    const words = []
+
+    for(let word of str.split(' ')){
+        words.push(word[0].toUpperCase() + word.slice(1))
     }
-    return str.join(' ')
+    return words.join(' ')
 }
 console.log(capitalize('my wild dog is called a beast'))
